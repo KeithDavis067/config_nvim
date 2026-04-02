fc4c34d42060b668156eae41246e886b5e437a8f
Keith W. Davis <kdavis10@nd.edu>
2026-04-02 09:30:06 -0400

    switched to fugitive.

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,21 +1,3 @@
 return {
-  "TimUntersberger/neogit",
-  cmd = "Neogit",
-  config = function()
-    require("neogit").setup({
-      kind = "split", -- opens neogit in a split
-      signs = {
-        -- { CLOSED, OPENED }
-        section = { "", "" },
-        item = { "", "" },
-        hunk = { "", "" },
-      },
-      integrations = { diffview = true }, -- adds integration with diffview.nvim
-    })
-  end,
-
-  {
-    "sindrets/diffview.nvim",
-    cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewToggleFiles", "DiffviewFocusFiles" },
-  },
+  { "tpope/vim-fugitive" },
 }
